83b504408021c9d31bab7124d4c3ce689d8febf7
mashhoodrehman <mashhoodr.rehman@gmail.com>
2025-09-16 17:13:32 +0500

    SMS service now completed

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const smsRoutes = require('./routes/smsRoute');
 
 const app = express();
-const PORT = 3000;
+const PORT = 9000;
 
 app.use(cors());
 app.use(express.json());
